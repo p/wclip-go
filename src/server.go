e51ced76137520a40c46c3b6343e18cfda1e6f87
Oleg Pudeyev <code@olegp.name>
2020-10-26 19:36:49 -0400

    skeleton

---
+++ src/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+  //"errors"
+  "fmt"
+  "github.com/gin-gonic/gin"
+  "os"
+  "strconv"
+  "sync"
+  //"io"
+  "io/ioutil"
+  "log"
+  "regexp"
+  "strings"
+  "time"
+
+  bolt "go.etcd.io/bbolt"
+  "net/http"
+)
+
+var http_user, http_password string
+var db *bolt.DB
+
+func get(c *gin.Context) {
+}
+
+func set(c *gin.Context) {
+}
+
+func set_cors_headers(c *gin.Context) {
+  c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+  c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+}
+
+func main() {
+  var err error
+
+  http_user = os.Getenv("HTTP_USER")
+  http_password = os.Getenv("HTTP_PASSWORD")
+  if http_user == "" && http_password != "" {
+    log.Fatal("HTTP_PASSWORD was specified but HTTP_USER was not, they need to be given together")
+  }
+  if http_user != "" && http_password == "" {
+    log.Fatal("HTTP_USER was specified but HTTP_PASSWORD was not, they need to be given together")
+  }
+
+  db_path := os.Getenv("DB_PATH")
+  if db_path == "" {
+    db_path = "otpbase.db"
+  }
+  db, err = bolt.Open(db_path, 0600, nil)
+  if err != nil {
+    log.Fatal("Error opening database")
+  }
+  defer db.Close()
+
+  db.Update(func(tx *bolt.Tx) error {
+    b, err := tx.CreateBucketIfNotExists([]byte("apps"))
+    if err != nil {
+      log.Fatal("Cannot create apps bucket")
+    }
+    b = b
+    return nil
+  })
+
+  // Disable Console Color
+  // gin.DisableConsoleColor()
+
+  debug := os.Getenv("DEBUG")
+  if debug == "" {
+    gin.SetMode(gin.ReleaseMode)
+  }
+
+  // Creates a gin router with default middleware:
+  // logger and recovery (crash-free) middleware
+  router := gin.Default()
+
+  //router.LoadHTMLGlob("views/*.html")
+
+  //router.Use(gin.Recovery())
+
+  router.GET("/*path", get)
+  router.POST("/*path", set)
+  router.PUT("/*path", set)
+  router.GET("/robots.txt", robots_txt)
+
+  // By default it serves on :8080 unless a
+  // PORT environment variable was defined.
+  port := os.Getenv("PORT")
+  var iport int
+  if port == "" {
+    iport = 8093
+  } else {
+    iport, err = strconv.Atoi(port)
+    if err != nil {
+      log.Fatal(err)
+    }
+  }
+  router.Run(fmt.Sprintf(":%d", iport))
+  // router.Run(":3000") for a hard coded port
+}
